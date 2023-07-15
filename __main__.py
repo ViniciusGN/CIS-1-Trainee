87823c6e78edd8a7b14bbd33823685cac9e586fd
ViniciusGN <170115437@aluno.unb.br>
2023-07-15 18:10:15 -0300

    part III included

--- __main__.py
+++ __main__.py
@@ -19,8 +19,8 @@ def observar_crimes_area(df):
     cod_area = cod_area.sort_values(by="AREA_ID", ascending=False) # Ordenar por código
 
     cod_area_plt = cod_area['AREA_NAME'].value_counts()
-    cod_area_plt.plot(kind='bar', figsize=(12,9))
-    plt.title('Frequência de Códigos de Área')
+    cod_area_plt.plot(kind='bar', figsize=(10,8))
+    plt.title('Frequência de Crimes por Área')
     plt.xlabel('Área')
     plt.ylabel('Quantidade de Ocorrências')
 
@@ -30,20 +30,84 @@ def observar_crimes_area(df):
     cod_area = cod_area.drop_duplicates() # Remover duplicatas
     cod_area.to_csv('cod_areas.csv', index=False) # Salvar em um novo arquivo
 
-if __name__ == '__main__':
-#------------------------------------------------ PARTE I
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("Parte I: Data Acquisition")
-    print("Los Angeles Crime Dataset (2020 -- Present)\n\n")
-    crimes = pd.read_csv('Crime_Data_from_2020_to_Present.csv') #abrir o CSV
+def observar_crimes_tipo(df):
+    id_crime= df["Crm Cd"]
+    desc_crime = df["Crm Cd Desc"]
 
+    # Criar um novo DataFrame com as colunas separadas
+    cod_crime = pd.DataFrame({'Crm_Id': id_crime, 'Crm_Desc': desc_crime})
+    cod_acod_crime = cod_crime.sort_values(by="Crm_Id", ascending=False) # Ordenar por código
+
+    cod_crime_plt = cod_acod_crime['Crm_Desc'].value_counts()
+    cod_crime_plt.plot(kind='bar', figsize=(10,8))
+    plt.title('Frequência de Crimes por Tipo')
+    plt.xlabel('Tipo de Crime')
+    plt.ylabel('Quantidade de Ocorrências')
+
+    plt.show()
+
+    # Salvar apenas os valores únicos-> Uma bilioteca de id para tipo de crime
+    cod_crime = cod_crime.drop_duplicates() # Remover duplicatas
+    cod_crime.to_csv('cod_areas.csv', index=False) # Salvar em um novo arquivo
+
+def observar_crimes_vitima(df):
+    # Sabemos que há colunas em que há associação de código por descrição. Vamos separar essas colunas em outro arquivo
+    Vict_Sex = df["Vict Sex"]
+    Vict_Age = df["Vict Age"]
+
+    type_vict = pd.DataFrame({'Vict Sex': Vict_Sex, 'Vict Age': Vict_Age})
+    type_vict = type_vict.sort_values(by="Vict Sex", ascending=False)
+
+    masculino = type_vict[type_vict['Vict Sex'] == 'M']
+    feminino = type_vict[type_vict['Vict Sex'] == 'F']
+    nao_declarado = type_vict[type_vict['Vict Sex'] == 'X']
+    
+    # Configurar a figura e os subplots
+    fig, axs = plt.subplots(3, 1, figsize=(8, 12), sharex=True)
+
+    # Histograma para sexo masculino
+    axs[0].hist(masculino['Vict Age'], bins=10, color='blue', alpha=0.7)
+    axs[0].set_title('Distribuição de Idade - Masculino')
+    axs[0].set_ylabel('Frequência')
+
+    # Histograma para sexo feminino
+    axs[1].hist(feminino['Vict Age'], bins=10, color='pink', alpha=0.7)
+    axs[1].set_title('Distribuição de Idade - Feminino')
+    axs[1].set_ylabel('Frequência')
+
+    # Histograma para sexo não declarado
+    axs[2].hist(nao_declarado['Vict Age'], bins=10, color='gray', alpha=0.7)
+    axs[2].set_title('Distribuição de Idade - Não Declarado')
+    axs[2].set_xlabel('Idade')
+    axs[2].set_ylabel('Frequência')
+
+    # Ajustar espaçamento entre os subplots
+    plt.tight_layout()
+
+    # Exibir o gráfico
+    plt.show()
+
+def ML_tipo_crime(df):
+    pass
+
+def ML_qual_sera_vitima(df):
+    pass
+
+def ML_onde_ocorrera(df):
+    pass
+
+def parte_i():
+    print("Parte I: Data Acquisition")
+    print("Los Angeles Crime Dataset (2020 -- Present)")
+
+def menu_1(crimes):
 #------------------------------------------------ PARTE II
     op = 1
-    while (op in range(1,6)):
+    while (op in range(0,6)):
         print("Parte II: Data Knowing")
-        print("Item b - Para realizar uma avaliação rápida do dataset, escolha um número (1-5):\n1 - Ver as primeiras linhas")
-        print("2 - o número de linhas e colunas do DataFrame\n3 - informações sobre as colunas e seus tipos de dados\n4 - estatísticas descritivas para as colunas numéricas")
-        print("5 - Continuar")
+        print("Para realizar uma avaliação rápida do dataset, escolha um número (1-5):\n1 - Ver as primeiras linhas")
+        print("2 - Ver o número de linhas e colunas do DataFrame\n3 - informações sobre as colunas e seus tipos de dados\n4 - estatísticas descritivas para as colunas numéricas")
+        print("5 - Continuar\n0 - Sair do codigo.")
 
         try:
             op = int(input("Digite o número: "))
@@ -63,28 +127,29 @@ if __name__ == '__main__':
         elif op == 4:
             print(crimes.describe())  # Gera estatísticas descritivas para as colunas numéricas
             limpar_tela()
-        elif op<1 or op>5:
+        elif op<0 or op>6:
             print("Opção inválida")
             op = 1
             limpar_tela()
-        else:
-            print("Continuando...")
+        elif op == 5:
+            limpar_tela()
             print("---Verificando as colunas---")
             colunas = crimes.columns
             print('Colunas: ', colunas)
             # Verificando crimes por area
-            limpar_tela()
             break
-            
-    # Verificando valores nulos
-    # print(crimes.isnull().sum())
+        elif op == 0:
+            print("Saindo do código...")
+            exit()
+
+def menu_2(crimes):
 #------------------------------------------------ PARTE III
     op2 = 1
-    while (op2 in range(1,6)):
+    while (op2 in range(0,5)):
         print("Parte III: Data Exploration")
-        print("Item b - Para realizar explorar o dataset, escolha um número (1-5):\n1 - Observar a frequencia de crimes por area")
-        print("2 - Empty\n3 - Empty\n4 - Empty")
-        print("5 - Continuar")
+        print("Para realizar explorar o dataset, escolha um número (1-5):\n1 - Observar a frequencia de crimes por area")
+        print("2 - Observar a frequencia de crimes por tipo\n3 - Observar a frequencia do perfil das vítimas")
+        print("4 - Continuar\n0 - Sair do codigo.")
         
         try:
             op2 = int(input("Digite o número: "))
@@ -92,19 +157,89 @@ if __name__ == '__main__':
             print("Entrada inválida. Tente novamente.")
 
         if op2 == 1:
-            observar_crimes_area(crimes)  
+            observar_crimes_area(crimes) 
+            print("Observamos que a maior parte dos crimes em LA ocorrem no Centro da Cidade.")
+            print("Observamos que a menor parte dos crimes em LA ocorrem no bairro Footnill.")
             limpar_tela()
+
         elif op2 == 2:
+            observar_crimes_tipo(crimes)
+            print("Observamos que a maior parte dos crimes em LA são de roubo.")
+            print("Observamos que a menor parte dos crimes em LA são de homicídio.")
             limpar_tela()
         elif op2 == 3:
+            observar_crimes_vitima(crimes)
+            print("Observamos que a maior parte das vítimas são homens.")
+            print("Observamos que a faixa étaria masculina mais atingida é de 20 a 30 anos.")
+            print("Observamos que a faixa étaria feminina mais atingida é de 20 a 30 anos.")
+            print("Observamos que a faixa étaria não declarada mais atingida é a proxima de 0 anos.")
             limpar_tela()
-        elif op2 == 4:
-            limpar_tela()
-        elif op2<1 or op2>5:
+        elif op2<0 or op2>5:
             print("Opção inválida")
             op2 = 1
             limpar_tela()
-        else:
-            print("---Observar a frequencia de crimes por area---")
-              
+        elif op2 == 0:
+            print("Saindo do código...")
+            exit()
+        elif op2 == 4:
+            print("Continuando...")
+            limpar_tela()
+            break
 
+def menu_3(crimes):
+    pass
+
+if __name__ == '__main__':
+#------------------------------------------------ PARTE I
+    os.system('cls' if os.name == 'nt' else 'clear')
+    parte_i()
+    print("Abrindo o DataFrame...\n\n")
+    crimes = pd.read_csv('Crime_Data_from_2020_to_Present.csv') #abrir o CSV
+
+# -------------------------Menu principal
+    op = 1
+    while (op in range(0,6)):
+        print("Menu principal:")
+        print("1 - Apresentação sequencial do trabalho:")
+        print("2 - Parte I: Data Acquisition")
+        print("3 - Parte II: Data Knowing")
+        print("4 - Parte III: Data Exploration")
+        print("5 - Parte IV: Machine Learning")
+        print("0 - Sair do codigo.")
+        try:
+            op = int(input("Digite o número: "))
+        except ValueError:
+            print("Entrada inválida. Tente novamente.")
+        if op == 1:
+            limpar_tela()
+            menu_1(crimes)
+            menu_2(crimes)
+            menu_3(crimes)
+            limpar_tela()
+        if op == 2:
+            limpar_tela()
+            parte_i()
+            limpar_tela()    
+        if op == 3:
+            limpar_tela()
+            menu_1(crimes)
+            limpar_tela()
+        if op == 4:
+            limpar_tela()
+            menu_2(crimes)
+            limpar_tela()
+        if op == 5:
+            limpar_tela()
+            menu_3(crimes)
+            limpar_tela()
+        if op == 0:
+            print("Saindo do código...")
+            exit()
+        elif op<0 or op>5:
+            print("Opção inválida")
+            op = 1
+            limpar_tela()
+    
+
+    # Verificando valores nulos
+    # print(crimes.isnull().sum())
